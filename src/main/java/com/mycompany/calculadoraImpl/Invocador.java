18d2f8721c6c1c6d7b8c3698b796aaba3ecc3c0a
usuario <usuario@equipo>
2015-02-15 20:59:44 -0500

    PrimerComit

--- src/main/java/com/mycompany/calculadoraImpl/Invocador.java
+++ src/main/java/com/mycompany/calculadoraImpl/Invocador.java
@@ -18,15 +18,30 @@ public class Invocador {
 
     public Invocador() {
         comandos= new HashMap<>();
-        comandos.put("-", new ComandoResta());
-        comandos.put("+", new ComandoSuma());
+        comandos.put("-"    ,   new ComandoResta());
+        comandos.put("+"    ,   new ComandoSuma());
+        comandos.put("*"    ,   new ComandoMultiplicar());
+        comandos.put("/"    ,   new ComandoDivision());
+        comandos.put("%"    ,   new ComandoCociente());
+        comandos.put("raiz2",   new ComandoRaiz2());
+        comandos.put("raiz" ,   new ComandoRaiz());
+        comandos.put("^"    ,   new ComandoElevar());
+        comandos.put("log10",   new ComandoLogaritmo10());
+        comandos.put("sen"  ,   new ComandoSeno());
+        comandos.put("cos"  ,   new ComandoCoseno());
+        comandos.put("tang" ,   new ComandoTangente());
+        
+        
     }
     
-    public double ejecutar(double op1, double op2, String operador){
+    public double ejecutarUnitaria(double op1, String operador){
+        Command cmd=comandos.get(operador);
+        return cmd.ejecutar(op1);
+    }
+    
+    public double ejecutarBinaria(double op1, double op2, String operador){
         Command cmd=comandos.get(operador);
         return cmd.ejecutar(op1, op2);
-    }
-    
-    
+    }    
     
 }
